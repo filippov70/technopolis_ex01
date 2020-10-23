33e98bf78504a83f7c45e8c0468d3ec251aa67bd
vgfilippov <vgfilippov@localhost.localdomain>
2020-10-23 19:10:23 +0700

    change text in marker

--- map.js
+++ map.js
@@ -3,13 +3,13 @@ function loadMap() {
   // первый слой карты - подложка OSM
   L.tileLayer(
     'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 22,
+      maxZoom: 26,
       attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
     }).addTo(map);
 
     // ещё слой
     var marker = L.marker([54.941597, 83.185505]).addTo(map); // координаты можно узнать при помощи QGIS
-    marker.bindPopup("<b>Шаверма!</b><br>Брат-джан готовит отличную шавуху!");
+    marker.bindPopup("<b>Шаверма the best!</b><br>Брат-джан готовит отличную шавуху!");
 
     let xhr = new XMLHttpRequest();
     xhr.open('GET', 'data.geojson');
