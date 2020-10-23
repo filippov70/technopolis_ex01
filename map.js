e9e48936e4052701407e7194ce4ec38e906c5841
filippov70 <filippov70@gmail.com>
2020-10-23 13:25:50 +0700

    add vector layer with marker

--- map.js
+++ map.js
@@ -1,10 +1,13 @@
 function loadMap() {
-  var map = L.map('map').setView([54.94, 83.19], 15); // координаты центра карты и ее масштаь
+  var map = L.map('map').setView([54.94, 83.19], 16); // координаты центра карты и ее масштаб
   // первый слой карты - подложка OSM
   L.tileLayer(
     'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
+      maxZoom: 22,
       attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
     }).addTo(map);
 
+    // ещё слой
+    var marker = L.marker([54.941597, 83.185505]).addTo(map); // координаты можно узнать при помощи QGIS
+    marker.bindPopup("<b>Шаверма!</b><br>Брат-джан готовит отличную шавуху!")
 }
