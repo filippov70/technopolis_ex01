601ba1dc649f767e1d4b746d70bc21dfbf01f4ce
filippov70 <filippov70@gmail.com>
2020-10-23 14:39:00 +0700

    add geojson file

--- map.js
+++ map.js
@@ -9,5 +9,26 @@ function loadMap() {
 
     // ещё слой
     var marker = L.marker([54.941597, 83.185505]).addTo(map); // координаты можно узнать при помощи QGIS
-    marker.bindPopup("<b>Шаверма!</b><br>Брат-джан готовит отличную шавуху!")
+    marker.bindPopup("<b>Шаверма!</b><br>Брат-джан готовит отличную шавуху!");
+
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', 'data.geojson');
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+    if (xhr.status !== 200) return
+      L.geoJSON(xhr.response).addTo(map);
+    };
+  xhr.send();
+}
+
+function addMarker(feature) {
+  // add marker
+  var myIcon = L.divIcon({
+    className: 'my-div-icon'
+  });
+  var marker = L.marker([feature.geometry.coordinates[1], feature.geometry.coordinates[0]]/*, myIcon*/).bindPopup(feature.properties.name);
+  markersLayer.addLayer(marker);
+  markersLayer.addTo(map);
+  map.setView([feature.geometry.coordinates[1], feature.geometry.coordinates[0]], 17);
 }
